a817d91aaaf93fb4d9bd25ddd3702e07cd1ef868
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-04 22:09:52 +0100

    Program updated

---
+++ JavaProgramming/src/SimpleJava.java
@@ -0,0 +1,11 @@
+
+public class SimpleJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to JavaPrograming");
+		System.out.println("It\na\nhigh\nlevel\nLanguage");
+		
+	}
+
+}
--- JavaProgramming/src/Test.java
+++ JavaProgramming/src/Test.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-		int a = 10;
+		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Number is : " + a );
+		int a1;
+		int b2;
+		int total;
+		
+		System.out.print("Enter number1: ");
+		a1 = input.nextInt();
+		
+		System.out.print("Enter number2: ");
+		b2 = input.nextInt();
+		
+		total = a1 + b2;
+		
+		System.out.printf("Total is : %d%n", total);
 	}
 
 }
