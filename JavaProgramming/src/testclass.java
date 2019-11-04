c15df83820d4847c8f241f2540da588222bafb0f
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-04 21:49:50 +0100

    TestingJavaProgram

---
+++ JavaProgramming/src/testclass.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ghulam Mustafa
+ *
+ */
+public class testclass {
+
+	/**
+	 * 
+	 */
+	public testclass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hey");
+
+	}
+
+}
